19d46fd5696110ae8831d39afc7782a644b08144
Kurt Lewis <kurt.jlewis15@gmail.com>
2017-01-25 17:39:10 -0500

    Make 05-why-to-use-pointers more detailed

--- pointers/05-when-to-use-pointers/01.cpp
+++ pointers/05-when-to-use-pointers/01.cpp
@@ -6,11 +6,24 @@ comes great responsibility.
 
 In a lot of cases just creating an instance to your object, type, or struct on the stack
 will work fine and is preferable. Leave the heavy lifting to the OS.
+
+Polymorphic behavior can only be accomplished through pointers. More on that when we review OOP!
+
+Some functions require a pointer, often in C libraries. In these cases it's unavoidable to use a pointer.
+
+
+Othertimes you might need to pass a variable by reference as previously discussed. Almost always it's preferable to pass
+using the reference operator. It's safer, less work, and takes advantages of the nice things that your Operating System
+is so kind to give to you.
 */
 
+#include <iostream>
 
 // The actual definition of this class is elsewhere, and we won't #include it here
 class myObjectDefinedElsewhere;
+
+void passByPntr(int *ptr);
+void passByRefOperator(int &ref);
 
 int main()
 {
@@ -22,6 +35,24 @@ int main()
      */
     myObjectDefinedElsewhere *forwadDeclare;
 
+    int a = 2;
+    int *ptr = &a;
+    int myInt = 0;
+    passByPntr(ptr);
+    passByRefOperator(myInt);
+    std::cout << *ptr << " " << myInt << std::endl;
+
+    //delete ptr;
 
     return 0;
+}
+
+void passByPntr(int *ptr)
+{
+    *ptr = 5;
+}
+
+void passByRefOperator(int &ref)
+{
+    ref = 5;
 }
