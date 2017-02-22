a75b6a97de4f497cb582d4ffbfc70a4ced7e5891
Zach Collins <collinzy@mail.uc.edu>
2017-02-21 19:01:15 -0500

    add second example

---
+++ oop/02-basics/02.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+/* This is a class with some better practices. The data is private. */
+class Rectangle
+{
+private:
+    /* These are private members of the class Rectangle.
+     * They can only be seen from inside the class, and by friend classes. */
+    int length;
+    int width;
+public:
+    /* These functions can tell us about the contents of the class. */
+    int GetLength() { return length; };
+    int GetWidth() { return width; };
+    int GetArea() { return length * width; };
+    
+    /* These functions can set the values of the variables in the class. */
+    int SetLength(int l) { length = l; };
+    int SetWidth(int w) { width = w; };
+
+    /* This is a constructor. It is used to initialize the class. */
+    Rectangle() { length = 0; width = 0; };
+
+    ~Rectangle() { /* This is a destructor. We can clean it up here. */ };
+};
+
+int main()
+{
+    Rectangle r;
+    std::cout << "Initial length: " << r.GetLength() << std::endl;
+
+    /* We can access parts of the rectangle using the functions. */
+    r.SetLength(3);
+    r.SetWidth(4);
+
+    /* Now we can see that we have modified the rectangle. */
+    std::cout << "length: " << r.GetLength() << " width: " << r.GetWidth() << 
+        " area: " << r.GetArea() << std::endl;
+}
+
